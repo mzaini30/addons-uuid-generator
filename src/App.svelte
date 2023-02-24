7b75a0f99fc5c069fe252bd0acd733be1fd1b440
Zen <muhzaini30@gmail.com>
2023-02-24 21:34:21 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Router from "svelte-spa-router";
+  // @ts-ignore
+  import routes from "virtual:generated-pages-svelte";
+  import "./tailwind.css";
+</script>
+
+<Router {routes} />
+
+<style>
+  :global(*) {
+    word-wrap: break-word;
+  }
+  /* chrome extension */
+  /* :global(body) {
+    width: 350px;
+    font-size: 16px;
+  } */
+</style>
